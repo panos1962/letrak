66a63b42c91f09299fc85568127cc107f7b403e8
panos1962 <panos1962@gmail.com>
2020-04-18 22:27:43 +0300

    modifications

--- www/imerisio/main.js
+++ www/imerisio/main.js
@@ -7,6 +7,12 @@ const letrak =
 require('../lib/letrak.js');
 const imerisio = {};
 
+imerisio.misc = {
+	'filtraHideTitle': 'Απόκρυψη φίλτρων',
+	'filtraShowTitle': 'Εμφάνιση φίλτρων',
+};
+	
+
 pnd.domInit(() => {
 	pnd.
 	toolbarSetup().
@@ -36,6 +42,17 @@ imerisio.toolbarSetup = () => {
 	toolbarCenterSetup().
 	xristisSetup();
 
+	if (letrak.noXristis())
+	return imerisio;
+
+	pnd.toolbarLeftDOM.
+	append(imerisio.filtraTabDOM = letrak.tabDOM().
+	append('Φίλτρα').
+	on('click', (e) => {
+		e.stopPropagation();
+		imerisio.filtraToggle(true);
+	}));
+
 	return imerisio;
 };
 
@@ -46,23 +63,130 @@ imerisio.ribbonSetup = () => {
 	return imerisio;
 };
 
+///////////////////////////////////////////////////////////////////////////////@
+
 imerisio.ofelimoSetup = () => {
 	if (letrak.noXristis())
 	return imerisio.welcome();
 
-	pnd.ofelimoDOM.
+	pnd.bodyDOM.
 	append(imerisio.panelDOM = $('<div>').
-	addClass('panel').
-	text('Panel')).
+	attr('id', 'panel'));
+
+	pnd.ofelimoDOM.
 	append(imerisio.browserDOM = $('<div>').
-	addClass('browser').
-	text('Imerisio'));
+	addClass('browser'));
+
+	imerisio.
+	panelSetup().
+	browserSetup();
 
 let i;
 for (i = 0; i < 100; i++)
 imerisio.browserDOM.
 append($('<div>').text(i));
 
+	return imerisio;
+};
+
+imerisio.panelSetup = () => {
+	imerisio.panelDOM.
+
+	append($('<div>').
+	addClass('filtroPedio').
+	append($('<label>').
+	attr('for', 'imerominiaFiltro').
+	text('Ημερομηνία')).
+	append(imerisio.imerominiaFiltroDOM = $('<input>').
+	attr('id', 'imerominiaFiltro').
+	addClass('pnd-imerominiaInput').
+	datepicker())).
+
+	append($('<div>').
+	addClass('filtroPedio').
+	append($('<label>').
+	attr('for', 'ipiresiaFiltro').
+	text('Υπηρεσία')).
+	append(imerisio.ipiresiaFiltroDOM = $('<input>').
+	attr('id', 'ipiresiaFiltro').
+	addClass('letrak-ipiresiaInput')));
+
+	imerisio.panelDOM.dialog({
+		'title': 'Κριτήρια επιλογής',
+		'autoOpen': false,
+
+		'width': 'auto',
+		'height': 'auto',
+		'position': {
+			'my': 'left+50 top+60',
+			'at': 'left top',
+		},
+
+		'open': function() {
+			imerisio.filtraTabDOM.data('status', 'visible');
+			imerisio.filtraToggle();
+		},
+
+		'show': {
+			'effect': 'drop',
+			'direction': 'up',
+			'duration': 100,
+		},
+
+		'close': function() {
+			imerisio.filtraTabDOM.data('status', 'hidden');
+			imerisio.filtraToggle();
+		},
+
+		'hide': {
+			'effect': 'drop',
+			'direction': 'up',
+			'duration': 100,
+		},
+	});
+
+	return imerisio;
+};
+
+imerisio.filtraToggle = function(act) {
+	if (imerisio.filtraDisabled())
+	imerisio.filtraEnable(act);
+
+	else
+	imerisio.filtraDisable(act);
+
+	return imerisio;
+};
+
+imerisio.filtraEnable = function(act) {
+	imerisio.filtraTabDOM.
+	removeClass('filtraTabOff').
+	attr('title', imerisio.misc.filrtaHideTitle);
+
+	if (!act)
+	return imerisio;
+
+	imerisio.filtraDOM.dialog('open');
+	return imerisio;
+};
+
+imerisio.filtraDisable = function(act) {
+	imerisio.filtraTabDOM.
+	addClass('filtraTabOff').
+	attr('title', imerisio.misc.filrtaHideTitle);
+
+	if (!act)
+	return imerisio;
+
+	imerisio.filtraDOM.dialog('close');
+	return imerisio;
+};
+
+imerisio.filtraDisabled = function() {
+	return (imerisio.filtraTabDOM.data('status') === 'hidden');
+};
+
+imerisio.browserSetup = () => {
 	return imerisio;
 };
 
