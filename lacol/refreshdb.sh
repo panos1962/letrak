a17df9598682bf71b715079fe3812dc471718a6d
panos1962 <panos1962@gmail.com>
2020-05-13 18:33:10 +0300

    modifications

--- lacol/refreshdb.sh
+++ lacol/refreshdb.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-local/createdb.sh | MYSQL_PWD="xxx" mysql -u root &&
-awk -f local/dbload.awk local/data/orario.tsv
+lacol/createdb.sh | MYSQL_PWD="xxx" mysql -u root &&
+awk -f lacol/dbload.awk lacol/data/orario.tsv
