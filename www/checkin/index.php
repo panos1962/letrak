cc1f155b19cff3802f60381288558e0c74172e61
panos1962 <panos1962@gmail.com>
2024-03-25 20:58:03 +0200

    modifications

--- www/checkin/index.php
+++ www/checkin/index.php
@@ -16,7 +16,7 @@
 // @FILE END
 //
 // @DESCRIPTION BEGIN
-// Σελίδα αναζήτησης σθμβάντων (χτυπημάτων καρτών). Σε αυτήν τη σελίδα ο χρήστης
+// Σελίδα αναζήτησης συμβάντων (χτυπημάτων καρτών). Σε αυτήν τη σελίδα ο χρήστης
 // καθορίζει κριτήρια αναζήτησης συμβάντων (ημερομηνία, διάστημα ωρών, υπηρεσίες
 // κλπ) και το πρόγραμμα συλλέγει όλα τα σχετικά συμβάντα, τα οποία παρουσιάζει
 // με τη μορφή λογιστικού φύλλου.
@@ -34,7 +34,14 @@ require_once("../../local/conf.php");
 require_once(LETRAK_BASEDIR . "/www/lib/letrak.php");
 
 pandora::
-session_init()::
+session_init();
+
+if (anonimo()) {
+	header("Location: ../isodos");
+	exit(0);
+}
+
+pandora::
 document_head([
 	"title" => "Checkin",
 	"css" => [
@@ -43,22 +50,50 @@ document_head([
 		"../lib/letrak",
 		"main",
 	],
-]);
+])::
+database();
 
-pandora::
-document_body();
+$imerominia = pandora::parameter_get("imerominia");
 
-?>
-<div id="kritiriaForma">
-	<div class="letrak-inputLine">
-		<label for="kritiriaFormaImerominia">
-			Ημερομηνία
-		</label>
-		<input id="kritiriaFormaImerominia" tabindex="-1">
-	</div>
-</div>
-<?php
+if (!$imerominia)
+$imerominia = date("Y-m-d");
 
-pandora::
-document_close();
-?>
+$ktirio = pandora::parameter_get("ktirio");
+
+if (!$ktirio)
+$ktirio = "NDM";
+
+$apo = pandora::parameter_get("apo");
+
+if (!$apo)
+$apo = "00:00:00";
+
+$eos = pandora::parameter_get("eos");
+
+if (!$eos)
+$eos = "23:59:59";
+
+$cmd = "lib/checkin.sh";
+$cmd .= " -x 'tmp/" . $checker . "'";
+$cmd .= " -d '" . $imerominia . "'";
+$cmd .= " -f '" . $apo . "'";
+$cmd .= " -t '" . $eos . "'";
+
+system($cmd);
+header("Location: tmp/" . $checker . ".xlsx");
+
+
+function anonimo() {
+	global $checker;
+
+	if (!array_key_exists("letrak_session_ipalilos", $_SESSION))
+	return TRUE;
+
+	$checker = json_decode($_SESSION["letrak_session_ipalilos"]);
+	$checker = $checker->kodikos;
+
+	if (!$checker)
+	return TRUE;
+
+	return FALSE;
+}
